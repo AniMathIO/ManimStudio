7ae1e1c5349a96d2cb57642a9ea5ae4a6e2f2021
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-12-11 04:23:41 +0200

    feat(project): initial commit

---
+++ form_ui.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QPushButton, QSizePolicy, QTextEdit,
+    QVBoxLayout, QWidget)
+
+class Ui_Main(object):
+    def setupUi(self, Main):
+        if not Main.objectName():
+            Main.setObjectName(u"Main")
+        Main.resize(396, 166)
+        self.verticalLayout_2 = QVBoxLayout(Main)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.textEdit = QTextEdit(Main)
+        self.textEdit.setObjectName(u"textEdit")
+        self.textEdit.setReadOnly(True)
+
+        self.verticalLayout_2.addWidget(self.textEdit)
+
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.pushButton_2 = QPushButton(Main)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+
+        self.verticalLayout.addWidget(self.pushButton_2)
+
+        self.pushButton = QPushButton(Main)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.verticalLayout.addWidget(self.pushButton)
+
+
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+
+
+        self.retranslateUi(Main)
+
+        QMetaObject.connectSlotsByName(Main)
+    # setupUi
+
+    def retranslateUi(self, Main):
+        Main.setWindowTitle(QCoreApplication.translate("Main", u"ManimStudio", None))
+        self.textEdit.setHtml(QCoreApplication.translate("Main", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"hr { height: 1px; border-width: 0; }\n"
+"li.unchecked::marker { content: \"\\2610\"; }\n"
+"li.checked::marker { content: \"\\2612\"; }\n"
+"</style></head><body style=\" font-family:'Noto Sans',''; font-size:10pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'Noto Sans'; font-size:20pt;\">Welcome to ManimStudio!</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'Noto Sans'; font-size:12pt;\">What would you like to do?</span></p></body></html>", None))
+        self.pushButton_2.setText(QCoreApplication.translate("Main", u"Create new project", None))
+        self.pushButton.setText(QCoreApplication.translate("Main", u"Open project", None))
+    # retranslateUi
+
