971b7a1ea4c03f9e7e44af1bcd308496885bbabf
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2024-01-04 17:46:32 +0200

    feat(docs): implemented initial doc configurations

---
+++ doc/conf.py
@@ -0,0 +1,20 @@
+project = "Manim Studio"
+copyright = "Kovács Bálint-Hunor"
+author = "Kovács Bálint-Hunor"
+release = "0.1"
+
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
